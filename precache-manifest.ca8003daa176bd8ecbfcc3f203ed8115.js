dd22da528a5624f2b387807cce12b60060a0d6f1
DomasJar <31046241+DomasJar@users.noreply.github.com>
2019-08-27 19:47:57 +0300

    Updates

---
+++ precache-manifest.ca8003daa176bd8ecbfcc3f203ed8115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62920e58b97fabe292b86ddd221b685c",
+    "url": "/SimplePage/index.html"
+  },
+  {
+    "revision": "3d6f3ef46261d4e7c2c3",
+    "url": "/SimplePage/static/css/2.38c1c84c.chunk.css"
+  },
+  {
+    "revision": "f930decaf60da396b372",
+    "url": "/SimplePage/static/css/main.b235255d.chunk.css"
+  },
+  {
+    "revision": "3d6f3ef46261d4e7c2c3",
+    "url": "/SimplePage/static/js/2.51b75913.chunk.js"
+  },
+  {
+    "revision": "f930decaf60da396b372",
+    "url": "/SimplePage/static/js/main.a017a940.chunk.js"
+  },
+  {
+    "revision": "6106eadbe0a53e188d80",
+    "url": "/SimplePage/static/js/runtime~main.f6f67237.js"
+  }
+]);
