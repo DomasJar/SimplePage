0e6b2e36e45d1c7c8cb0701154674794a2c941c5
DomasJar <31046241+DomasJar@users.noreply.github.com>
2019-08-28 20:39:28 +0300

    Ig logo fixed

--- src/components/Media.js
+++ src/components/Media.js
@@ -24,8 +24,8 @@ class Media extends Component {
                     <p className={classNames("cont-2", {"o--reveal-2": !this.state.visible})}>Media</p>
                     <div className={classNames("hidden", {"o--reveal": !this.state.visible})}>
                         <div className={"mcontainer"}>
-                            <img className={"pad-lr icon"} src={'res/Spotify_Icon_CMYK_Black.png'} alt={"icon"} onClick={()=> window.open("https://open.spotify.com/artist/5eDqoeZQwEBOIsCeMH1KRd?si=XOVeWx8ASAKRRgPONn4CbQ", "_blank")}></img>
-                            <img className={"pad-lr icon"} src={'res/Ig-Logo_May2016.png'} alt={"icon"} onClick={()=> window.open("https://www.instagram.com/domasjar/", "_blank")}></img>
+                            <img className={"pad-lr icon"} src={'res/Spotify_Icon_CMYK_Black.png'} alt={"spotify_icon"} onClick={()=> window.open("https://open.spotify.com/artist/5eDqoeZQwEBOIsCeMH1KRd?si=XOVeWx8ASAKRRgPONn4CbQ", "_blank")}></img>
+                            <img className={"pad-lr icon"} src={'res/Ig-Logo_May2016.png'} alt={"ig_icon"} onClick={()=> window.open("https://www.instagram.com/domasjar/", "_blank")}></img>
                         </div>
                     </div>
                 </div>
