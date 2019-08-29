1d5c1bf7f3f5411ed84ffa343113e847f29257a9
DomasJar <31046241+DomasJar@users.noreply.github.com>
2019-08-29 16:22:22 +0300

    Updates

---
+++ precache-manifest.b435e30d3a001c451fc10abce22ee58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4348b10cb383ab0decd021f43b8ef72",
+    "url": "/SimplePage/index.html"
+  },
+  {
+    "revision": "3d996240c3d127cdf9e9",
+    "url": "/SimplePage/static/css/2.38c1c84c.chunk.css"
+  },
+  {
+    "revision": "3e448105714b862a96c8",
+    "url": "/SimplePage/static/css/main.41afbd2b.chunk.css"
+  },
+  {
+    "revision": "3d996240c3d127cdf9e9",
+    "url": "/SimplePage/static/js/2.9a1040a6.chunk.js"
+  },
+  {
+    "revision": "3e448105714b862a96c8",
+    "url": "/SimplePage/static/js/main.763f853e.chunk.js"
+  },
+  {
+    "revision": "6106eadbe0a53e188d80",
+    "url": "/SimplePage/static/js/runtime~main.f6f67237.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SimplePage/precache-manifest.ddd0679977d045ceef9ef003925a7795.js"
+  "/SimplePage/precache-manifest.b435e30d3a001c451fc10abce22ee58c.js"
 );
 
 self.addEventListener('message', (event) => {
